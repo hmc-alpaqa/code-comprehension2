e17963daf198f3fad664553e62e00081b27ac04b
Diego Esparza <dae1@williams.edu>
2022-03-05 02:50:01 -0500

    1 indexed instead

--- challenge/views.py
+++ challenge/views.py
@@ -18,11 +18,11 @@ def login_or_new(request):
         for challenge in challenges:
             challenge.save()
 
-    if FactorTag.objects.count() == 0:
-        num_factors = [2, 4, 4]
-        for i in range(len(num_factors)):
-            for j in range(num_factors[i]):
-                factor_name = f"Factor_{j}"
+    num_factors = [2, 4, 4]
+    for i in range(len(num_factors)):
+        for j in range(num_factors[i]):
+            factor_name = f"Factor_{j+1}"
+            if FactorTag.objects.filter(tag__exact=factor_name).count() == 0:
                 challenge = ChallengeTag.objects.filter(tag__exact=challenge_names[i])[0]
                 factor = FactorTag(tag=factor_name, challenge_tag=challenge)
                 factor.save()
